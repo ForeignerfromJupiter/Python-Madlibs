657df66185fb0b18cc24e66a0594706a34028546
ForeignerfromJupiter <24405039+ForeignerfromJupiter@users.noreply.github.com>
2021-07-08 11:13:24 +0530

    Madlibs

---
+++ Adult.py
@@ -0,0 +1,24 @@
+name = input("Crush's Name: ")
+verb = input("Verb: ")
+room = input("Favorite room in the house: ")
+plural_noun = input("Plural noun: ")
+body_part1 = input("Body part: ")
+furniture = input("Piece of furniture: ")
+adj = input("Adjective: ")
+adverb = input("Adverb: ")
+number = input("Number: ")
+time = input("Time: ")
+body_part2 = input("Plural body part: ")
+artist = input("Favorite music artist: ")
+food = input("Favorite food: ")
+shoe_brand = input("Brand of shoes, plural: ")
+madlibs =f"You were suddenly feeling frisky so you asked {name} if she wanted to {verb} \
+in the {room} with you. Normally, you use this room to store your vast collection of {plural_noun}, but this time you felt like switching things up. \
+\
+You just know she was ready to go when she propped up her {body_part1} up on the {furniture} and gave you her {adj} bedroom eyes. \
+\
+After {adverb} going at it for {number} minutes, you both climaxed at the exact same time: {time}. But you looked deep into each other's {body_part2} and knew \
+exactly what the other one wanted: one more round. You put on {artist}'s greatest hits, lit a few {food} - scented candles and \
+knocked {shoe_brand} for more hours."
+print(madlibs)
+
